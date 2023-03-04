08425776b3ce25948c4b4938e550f2c141c7a32e
roycehoe <royceinbox@hotmail.com>
2023-03-04 21:46:40 +0800

    Feat; Init boilerplate code for property location SQLite database

---
+++ models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String
+
+from database import Base
+
+
+class PropertyLocation(Base):
+    __tablename__ = "property_location"
+
+    id = Column(Integer, primary_key=True, index=True)
+    description = Column(String)
+    block = Column(String)
+    steet_name = Column(String)
+    address = Column(String)
+    latitude = Column(String)
+    longitude = Column(String)
+    type = Column(String)
