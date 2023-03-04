b53436f31a89fc7dec893105b3fc04591f8757fc
roycehoe <royceinbox@hotmail.com>
2023-03-04 19:16:39 +0800

    Refactor; Shift all constants to constants folder

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 import requests
 import json
 
-from constants import BASE_URL, HDB_RESALE_BY_BUILDING_BLOCK_PATH, PPT_RESALE_BY_LAT_LON_PATH, RESALE_BLOCK_DEFAULT_PARAMS, RESALE_BLOCK_PATH, TOKEN, TOTAL_DISTRICTS
+from constants import (
+    BASE_URL,
+    HDB_RESALE_BY_BUILDING_BLOCK_PATH,
+    PPT_RESALE_BY_LAT_LON_PATH,
+    RESALE_BLOCK_DEFAULT_PARAMS,
+    RESALE_BLOCK_PATH,
+    TOKEN,
+    TOTAL_DISTRICTS,
+)
 
 
 def get_property_API_baseURL(type: str):
     if type == "hdb":
-        return f'{BASE_URL}/{HDB_RESALE_BY_BUILDING_BLOCK_PATH}'
-    return f'{BASE_URL}/{PPT_RESALE_BY_LAT_LON_PATH}'
+        return f"{BASE_URL}/{HDB_RESALE_BY_BUILDING_BLOCK_PATH}"
+    return f"{BASE_URL}/{PPT_RESALE_BY_LAT_LON_PATH}"
+
 
 def get_property_API_url(property_data):
     baseURL = get_property_API_baseURL(property_data["type"])
     latitude = property_data["latitude"]
     longitude = property_data["longitude"]
 
-    if (block := property_data.get("block")):
+    if block := property_data.get("block"):
         return f"{baseURL}?block={block}&latitude={latitude}&longitude={longitude}&token={TOKEN}"
     return f"{baseURL}?block={block}latitude={latitude}&longitude={longitude}&token={TOKEN}"
 
+
 def get_resale_block_data(district: int):
-    response = requests.get(f"{BASE_URL}/{RESALE_BLOCK_PATH}{RESALE_BLOCK_DEFAULT_PARAMS}&district={district}&token={TOKEN}")
+    response = requests.get(
+        f"{BASE_URL}/{RESALE_BLOCK_PATH}{RESALE_BLOCK_DEFAULT_PARAMS}&district={district}&token={TOKEN}"
+    )
     return response.json()
+
 
 def _init_resale_block_data(total_districts=TOTAL_DISTRICTS):
     resale_block_data = []
@@ -35,6 +48,7 @@ def get_resale_data(block_data: dict):
     api_url = get_property_API_url(block_data)
     response = requests.get(api_url).json()
     return [dict(t) for t in {tuple(d.items()) for d in response}]
+
 
 with open("resale_block_data.json", "r") as f:
     block_data = json.load(f)
