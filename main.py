dd2e0962cfc653f1a3a1e186c9bb28ffec99a9b9
roycehoe <royceinbox@hotmail.com>
2023-03-04 19:14:59 +0800

    Refactor; Shift all constants to constants folder

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 import requests
 import json
 
-TOTAL_DISTRICTS = 28
-TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjMsInVzZXJfaWQiOjMsImVtYWlsIjoicHVibGljQXBpUm9sZUBzbGEuZ292LnNnIiwiZm9yZXZlciI6ZmFsc2UsImlzcyI6Imh0dHA6XC9cL29tMi5kZmUub25lbWFwLnNnXC9hcGlcL3YyXC91c2VyXC9zZXNzaW9uIiwiaWF0IjoxNjc3OTAyMDgyLCJleHAiOjE2NzgzMzQwODIsIm5iZiI6MTY3NzkwMjA4MiwianRpIjoiNmI5NDFhZGRiNTg2N2M3NGQ5NGQxZTIwODFmZjc5MWMifQ.gE2yjDlRWuzK1y8aCUTnRMa8b5djMoh2DkyJBHtfUC0"
+from constants import BASE_URL, HDB_RESALE_BY_BUILDING_BLOCK_PATH, PPT_RESALE_BY_LAT_LON_PATH, RESALE_BLOCK_DEFAULT_PARAMS, RESALE_BLOCK_PATH, TOKEN, TOTAL_DISTRICTS
 
-BASE_URL = "https://developers.onemap.sg/publicapi/propsvc"
-RESALE_BLOCK_PATH = "retrieve_property_locations_within_district"
-HDB_RESALE_BY_BUILDING_BLOCK_PATH= "getHDBResaleByBuildingBlock"
-PPT_RESALE_BY_LAT_LON_PATH= "getHDBResaleByBuildingBlock"
-
-RESALE_BLOCK_DEFAULT_PARAMS = "?apartment=true&condo=true&executive_condo=true&executive_hdb=true&five_room_hdb=true&four_room_hdb=true&landed=true&lower_bound_date=20220304&lower_bound_price=0&multi_gen_hdb=true&one_room_hdb=true&three_room_hdb=true&two_room_hdb=true&upper_bound_date=20230304&upper_bound_price=200000000"
 
 def get_property_API_baseURL(type: str):
     if type == "hdb":
@@ -48,5 +41,3 @@ with open("resale_block_data.json", "r") as f:
 
 for i in range(10):
     something = get_resale_data(block_data[i])
-
-print(something)
